67a12813c09afc7b6e2013417d6a889b60935f63
Freek Gruntjes <freek@gruntjes.net>
2012-11-29 17:55:47 +0100

    Added option to disable locale route

--- src/WfkLocale/Service/ConfigFactory.php
+++ src/WfkLocale/Service/ConfigFactory.php
@@ -27,21 +27,38 @@ class ConfigFactory extends ZendConfigFactory
         $mode = !isset($config['wfklocale']['mode']) ? ModuleOptions::MODE_URLKEY : $config['wfklocale']['mode'];
         if ($mode == ModuleOptions::MODE_URLKEY && isset($config['router']))
         {
+			$localeEnabledRoutes = array();
+			$localeDisabledRoutes = array();
+			foreach($config['router']['routes'] as $key => $route)
+			{
+				if(isset($route['options']) && isset($route['options']['disable_locale']) && $route['options']['disable_locale'])
+				{
+					$localeDisabledRoutes[$key] = $route;
+				}
+				else
+				{
+					$localeDisabledRoutes[$key] = $route;
+				}
+			}
+
             $possibleLocales = $config['wfklocale']['enabled'];
             $possibleLocales = array_unique(array_values($possibleLocales));
-            $config['router']['routes'] = array(
-                'wfklocale-root' => array(
-                    'type' => 'Segment',
-                    'options' => array(
-                        'route'    => '/:locale',
-                        'constraints' => array(
-                            'locale' => '(' . implode('|', $possibleLocales) . ')'
-                        )
-                    ),
-                    'may_terminate' => false,
-                    'child_routes' => $config['router']['routes']
-                )
-            );
+            $config['router']['routes'] = array_merge(
+				array(
+					'wfklocale-root' => array(
+						'type' => 'Segment',
+						'options' => array(
+							'route'    => '/:locale',
+							'constraints' => array(
+								'locale' => '(' . implode('|', $possibleLocales) . ')'
+							)
+						),
+						'may_terminate' => false,
+						'child_routes' => $localeEnabledRoutes
+					)
+            	),
+				$localeDisabledRoutes
+			);
         }
 
         return $config;
